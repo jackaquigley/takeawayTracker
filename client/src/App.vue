7612de5e71e12880a64602e9794645217843e2eb
Jack Quigley <jackaquigley@gmail.com>
2020-05-22 20:57:29 +0100

    set up vue, and router

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+import {eventBus} from '@/main.js';
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="css" scoped>
+</style>
